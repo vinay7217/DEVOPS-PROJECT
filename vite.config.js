e4e2b339fdf84166238482ea3d724417ffaae79f
vinay7217 <vinaysomisetty123@gmail.com>
2025-08-08 23:40:35 +0530

    SECOND COMMITt

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/hello-world-vite-app/',
+  base: '/DEVOPS-PROJECT/',
   plugins: [react()],
 })
